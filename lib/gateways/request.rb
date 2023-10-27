c99499e8d2b58c13b6f74d963da8f1541d1d6b74
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-10-27 20:23:52 +0800

    fix: function exposure

--- lib/gateways/request.rb
+++ lib/gateways/request.rb
@@ -3,14 +3,14 @@
 module TravelRoute
   # Contains logic about http request
   class Request
-    def get(url)
+    def self.get(url)
       http_response = HTTP.get(url)
       Response.new(http_response).tap do |response|
         raise(response.error) unless response.successful?
       end
     end
 
-    def post(url, headers, body)
+    def self.post(url, headers, body)
       http_response = HTTP.headers(headers).post(url, json: body)
       Response.new(http_response).tap do |response|
         raise(response.error) unless response.successful?
