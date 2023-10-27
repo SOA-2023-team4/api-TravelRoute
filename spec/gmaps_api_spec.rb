02008b39cb0f7bbecc2278970e176356a0d811c5
hank liu <hankliucode@gmail.com>
2023-10-27 17:14:58 +0800

    tests: rewrite tests to accomodate the changes

--- spec/gmaps_api_spec.rb
+++ spec/gmaps_api_spec.rb
@@ -23,92 +23,47 @@ describe 'Tests Google Maps API library' do
 
   describe 'Place information' do
     it 'HAPPY: should provide correct place information' do
-      PLACES.each do |place|
-        query = TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).place_matches(place)
-        PLACE_RESULT[place]['matches'].each_with_index do |p, index|
-          _(query[index].id).must_equal p['place_id']
-          _(query[index].name).must_equal p['name']
-          _(query[index].formatted_address).must_equal p['formatted_address']
-        end
+      response = TravelRoute::GoogleMaps::Places::Api.new(GMAP_TOKEN).places_from_text_data(PLACE)
+      PLACE_RESULT['candidates'].zip(response['candidates']).each do |expected, generated|
+        _(expected['place_id']).must_equal generated['place_id']
+        _(expected['name']).must_equal generated['name']
       end
     end
 
     it 'SAD: should return empty array when place is not found' do
-      _(TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).place_matches('idoesntexist')).must_be_empty
+      response = TravelRoute::GoogleMaps::Places::Api.new(GMAP_TOKEN).places_from_text_data('notexistentplace')
+      _(response['candidates']).must_be_empty
     end
 
     it 'BAD: should raise exception when unauthorized' do
       _(proc do
-        TravelRoute::GoogleMapsApi.new('bad_token').place_matches(PLACES[0])
-      end).must_raise TravelRoute::ApiResponse::Errors::RequestDenied
+        TravelRoute::GoogleMaps::Places::Api.new('bad_token').places_from_text_data(PLACE)
+      end).must_raise TravelRoute::Response::Unauthorized
     end
   end
 
   describe 'Route information' do
     before do
-      @origin = TravelRoute::Place.new(PLACE_RESULT[PLACES[0]]['matches'][0])
-      @valid_destination = TravelRoute::Place.new(PLACE_RESULT[PLACES[1]]['matches'][0])
-      @invalid_destination = TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).place_matches('Rio de Janeiro')[0]
+      @origin = 'ChIJB7ZNzXI2aDQREwR22ltdKxE'
+      @valid_destination = 'ChIJmecsSO01aDQRGT_QzrFSwfA'
+      @invalid_destination = 'invaliddestinationId'
     end
 
     it 'HAPPY: should provide correct route information' do
-      route = TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).get_routes(@origin, @valid_destination)
-      route.each do |r|
-        _(r.origin.name).must_equal @origin.name
-        _(r.origin.id).must_equal @origin.id
-        _(r.destination.name).must_equal @valid_destination.name
-        _(r.destination.id).must_equal @valid_destination.id
-        _(r.distance_meters).must_be_kind_of Integer
-        _(r.duration).must_be_kind_of String
-      end
+      response = TravelRoute::GoogleMaps::Routes::Api.new(GMAP_TOKEN).route_data(@origin, @valid_destination)
+      _(response['routes']).wont_be_empty
     end
 
     it 'SAD: should raise exception when route not found' do
       _(proc do
-        TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).get_routes(@origin, @invalid_destination)
-      end).must_raise TravelRoute::GoogleMapsApi::Errors::RouteNotFound
+        TravelRoute::GoogleMaps::Routes::Api.new(GMAP_TOKEN).route_data(@origin, @invalid_destination)
+      end).must_raise TravelRoute::Response::BadRequest
     end
 
     it 'BAD: should raise exception when unauthorized' do
       _(proc do
-        TravelRoute::GoogleMapsApi.new('bad_token').get_routes(@origin, @valid_destination)
-      end).must_raise TravelRoute::ApiResponse::Errors::Unauthorized
-    end
-  end
-
-  describe 'Route Matrix information' do
-    before do
-      @nthu = TravelRoute::Place.new(PLACE_RESULT[PLACES[0]]['matches'][0])
-      @zoo = TravelRoute::Place.new(PLACE_RESULT[PLACES[2]]['matches'][0])
-      @big_city = TravelRoute::Place.new(PLACE_RESULT[PLACES[1]]['matches'][0])
-
-      @places = [@nthu, @zoo, @big_city]
-    end
-
-    it 'HAPPY: should return nearest destination' do
-      matrix = TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).get_route_matrix(@places)
-      _(matrix.nearest_from(@nthu).origin.name).must_equal @nthu.name
-      _(matrix.nearest_from(@nthu).origin.id).must_equal @nthu.id
-      _(matrix.nearest_from(@nthu).destination.name).must_equal @zoo.name
-      _(matrix.nearest_from(@nthu).destination.id).must_equal @zoo.id
-    end
-
-    it 'HAPPY: should return correct route' do
-      matrix = TravelRoute::GoogleMapsApi.new(GMAP_TOKEN).get_route_matrix(@places)
-      route = matrix.construct_route_from(@nthu)
-      _(route.size).must_equal @places.size - 1
-      route.each_with_index do |r, i|
-        _(r.origin.name).must_equal @places[i].name
-        _(r.origin.id).must_equal @places[i].id
-        _(r.destination.name).must_equal @places[i + 1].name
-        _(r.destination.id).must_equal @places[i + 1].id
-      end
-    end
-
-    it 'BAD: should raise exception when unauthorized' do
-      _(proc do
-        TravelRoute::GoogleMapsApi.new('bad_token').get_route_matrix(@places)
-      end).must_raise TravelRoute::ApiResponse::Errors::Unauthorized
+        TravelRoute::GoogleMaps::Routes::Api.new('bad_token').route_data(@origin, @valid_destination)
+      end).must_raise TravelRoute::Response::BadRequest
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,12 +10,16 @@ require 'yaml'
 require 'vcr'
 require 'webmock'
 
-require_relative '../lib/gmaps_api'
+require_relative '../lib/gateways/places_api'
+require_relative '../lib/gateways/routes_api'
 
 CONFIG = YAML.safe_load_file('config/secrets.yml')
 GMAP_TOKEN = CONFIG['MAPS_API_KEY']
-PLACE_RESULT = YAML.safe_load_file('spec/fixtures/places_results.yml')
-PLACES = %w[國立清華大學 巨城 新竹動物園].freeze
-ROUTE_RESULT = YAML.safe_load_file('spec/fixtures/routes_results.yml')
+
+PLACE = '清大'
+PLACE_RESULT = YAML.safe_load_file('spec/fixtures/places_from_text.yml')
+PLACE_DETAIL_RESULT = YAML.safe_load_file('spec/fixtures/place_detail.yml')
+ROUTE_RESULT = YAML.safe_load_file('spec/fixtures/route_data.yml')
+
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'gmaps_api'
