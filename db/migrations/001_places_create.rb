030aa7d05d5fa58f22db34a476e8347a5303a4c7
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-11-01 20:04:12 +0800

    feat: database migration file

---
+++ db/migrations/001_places_create.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:places) do
+      primary_key :id
+
+      String      :place_id, unique: true, null: false
+      String      :name, unique: true, null: false
+      String      :address
+      Float       :rating
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
