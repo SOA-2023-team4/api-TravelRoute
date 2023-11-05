3a7d6a046b6d51007f2d2b850675d23d232531b9
HankLiuCode <hankliucode@gmail.com>
2023-11-05 23:55:06 +0800

    feat: Repository for places and routes

---
+++ app/models/repositories/for.rb
@@ -0,0 +1,22 @@
+require_relative 'places'
+require_relative 'routes'
+
+module TravelRoute
+  module Repository
+    # Finds the right repository for an entity object or class
+    module For
+      ENTITY_REPOSITORY = {
+        Entity::Place => Places,
+        Entity::Route => Routes
+      }.freeze
+
+      def self.klass(entity_klass)
+        ENTITY_REPOSITORY[entity_klass]
+      end
+
+      def self.entity(entity_object)
+        ENTITY_REPOSITORY[entity_object.class]
+      end
+    end
+  end
+end
