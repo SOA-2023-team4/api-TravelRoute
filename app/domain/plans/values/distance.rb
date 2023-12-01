0f91bbe8b4a86ff3f4601287a62e3852a8b4bdfd
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-12-01 13:40:11 +0800

    refactor: move distance and duration estimation funtion to `lib` folder

--- app/domain/plans/values/distance.rb
+++ app/domain/plans/values/distance.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
+require_relative '../lib/distance_estimator'
+
 module TravelRoute
   module Value
     # Value of route's distance
     class Distance
+      include Mixins::DistanceEstimator
+
       def initialize(distance)
         @distance = distance
-      end
-
-      def to_m
-        @distance
-      end
-
-      def to_km
-        (to_m / 1000).ceil
-      end
-
-      def estimate_distance
-        @distance < 1000 ? [to_m, 'm'] : [to_km, 'km']
       end
     end
   end
