a28b69c1aeeb5807bc5b7a9510780aef3b60bbb9
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-12-31 17:17:07 +0800

    feat: add worker for stay time suggestion

---
+++ workers/stay_time_worker.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require_relative '../require_app'
+require_relative 'stay_time_reporter'
+require_relative 'reccommendation_monitor'
+require_app
+
+require 'figaro'
+require 'shoryuken'
+
+# Shoryuken worker class to clone repos in parallel
+module Background
+  # Shoryuken worker class to recommend attractions
+  class StayTimeWorker
+    # Environment variables setup
+    Figaro.application = Figaro::Application.new(
+      environment: ENV['RACK_ENV'] || 'development',
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+    def self.config = Figaro.env
+
+    Shoryuken.sqs_client = Aws::SQS::Client.new(
+      access_key_id: config.AWS_ACCESS_KEY_ID,
+      secret_access_key: config.AWS_SECRET_ACCESS_KEY,
+      region: config.AWS_REGION
+    )
+
+    include Shoryuken::Worker
+    shoryuken_options queue: config.STAYTIME_QUEUE_URL, auto_delete: true
+
+    def perform(_sqs_msg, request)
+      job = StayTimeReporter.new(self.class.config, request)
+      attraction = TravelRoute::Entity::Attraction.new(**symbolize_keys(job.attraction.to_h))
+
+      suggestion = request_time(attraction)
+      TravelRoute::Repository::Attractions.update(
+        TravelRoute::Entity::Attraction.new(**attraction.to_attr_hash.merge(suggestion))
+      )
+    end
+
+    def request_time(attraction)
+      chat = TravelRoute::OpenAi::Api.new(RecommendationWorker.config.OPENAI_API_KEY)
+        .get_time_to_stay(attraction.time_to_stay_prompt)
+      puts chat
+      JSON.parse(chat, symbolize_names: true)
+    end
+
+    def symbolize_keys(hash)
+      hash.to_h.transform_keys(&:to_sym).transform_values do |v|
+        v.is_a?(Hash) ? v.transform_keys(&:to_sym) : v
+      end
+    end
+  end
+end
