fa04da4b8eccdffe319122382b6e65478c82e343
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2023-12-31 17:22:04 +0800

    refactor: Add stay_time attribute to Attraction class

--- app/domain/plans/entities/attraction.rb
+++ app/domain/plans/entities/attraction.rb
@@ -10,13 +10,13 @@ module TravelRoute
     # Data structure for place information
     class Attraction < Dry::Struct
       include Dry.Types
-      attr_accessor :description
 
       attribute :place_id,        Strict::String
       attribute :name,            Strict::String
       attribute :address,         Strict::String
       attribute? :description,    Strict::String.optional
       attribute? :type,           Strict::String.optional
+      attribute? :stay_time,      Strict::Integer.optional
       attribute? :opening_hours,  Strict::Hash.optional
       attribute :rating,          Coercible::Float.optional
       attribute :location,        Hash.optional
@@ -29,9 +29,9 @@ module TravelRoute
           address:,
           type:,
           opening_hours:,
+          stay_time:,
           rating:,
-          latitude:,
-          longitude:
+          location:
         }
       end
 
@@ -49,6 +49,18 @@ module TravelRoute
 
       def city
         Value::Address.new(address).city
+      end
+
+      def reccommendation_prompt(num = 3, exclude = nil)
+        Value::OpenAiPrompt.reccommendation_prompt(self, num, exclude)
+      end
+
+      def time_to_stay_prompt
+        Value::OpenAiPrompt.time_to_stay_prompt(self)
+      end
+
+      def filled?
+        Value::SuggestedValue.new(stay_time, description).present?
       end
     end
   end
