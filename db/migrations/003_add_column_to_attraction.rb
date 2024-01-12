3805d0a1b920e01b848f2298f234cd8af9959603
Supakarn Jariyayong <sup.jariyayong@hotmail.com>
2024-01-12 13:46:01 +0800

    fix: fix db migration

--- db/migrations/003_add_column_to_attraction.rb
+++ db/migrations/003_add_column_to_attraction.rb
@@ -5,7 +5,6 @@ require 'sequel'
 Sequel.migration do
   change do
     add_column :attractions, :description, String, null: true, default: nil
-    add_column :attractions, :hour, Integer
-    add_column :attractions, :minute, Integer
+    add_column :attractions, :stay_time, Integer, null: true, default: nil
   end
 end
